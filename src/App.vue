92209879630373945e7d5b36ae9c5435b6c39b2f
April Day <april.day@gmail.com>
2021-09-02 10:18:07 +0100

    add tag basic template and some styling

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import "~@/assets/scss/vendors/bootstrap-vue/index";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/TagInput.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="tag-input">
+        <ul class="tags">
+            <li v-for="tag in tags" :key="tag" class="tag">
+                {{ tag }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+
+import {ref} from vue
+
+//import { defineComponent } from '@vue/composition-api'
+
+/*
+export default defineComponent({
+    setup() {
+        
+    },
+})
+*/
+export default {
+
+    setup(){
+        const tags = ref(['hello', 'world']);
+        return { tags }
+    }
+}
+    
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    display:flex;
+    align-items: center;
+    gap: 7px;
+    margin: 0;
+    padding: 0;
+}
+
+.tag {
+    background: rgb(250, 104, 104);
+    padding: 5px;
+    border-radius: 4px;
+    color: white;
+    white-space: nowrap;
+    transition: 0.1s ease background;
+}
+</style>
